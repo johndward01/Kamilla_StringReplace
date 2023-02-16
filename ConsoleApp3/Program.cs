cf7844170166277ed46b21422ab683cb81cbcd69
johndward01 <johndward01@gmail.com>
2023-02-16 16:09:09 -0600

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿static string ReplaceSpace(string str)
+{
+    return str.Replace(" ", "%20");
+}
+
+string oldString = "Jill Scott";
+var newString = ReplaceSpace(oldString);
+
+Console.WriteLine(newString);
